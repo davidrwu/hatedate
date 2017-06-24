b3e09af4a9244c76b6dcf9787e4d019202e36855
David Wu <David Wu>
2017-06-23 19:41:32 -0700

    almost there

---
+++ profile.js
@@ -0,0 +1,244 @@
+//Ramona's Firebase
+// var config = {
+//       apiKey: "AIzaSyCoMO4JICSt88ZIMpok7Jvv7bhyVIMYErw",
+//       authDomain: "datingproj-44fa5.firebaseapp.com",
+//       databaseURL: "https://datingproj-44fa5.firebaseio.com",
+//       projectId: "datingproj-44fa5",
+//       storageBucket: "datingproj-44fa5.appspot.com",
+//       messagingSenderId: "834628368468"
+//       };
+//       firebase.initializeApp(config);
+
+
+var config = {
+  apiKey: "AIzaSyCsk5WGqR1YlbaJiCnYbs1lgPgEQZAopr8",
+  authDomain: "hate-date.firebaseapp.com",
+  databaseURL: "https://hate-date.firebaseio.com",
+  projectId: "hate-date",
+  storageBucket: "hate-date.appspot.com",
+  messagingSenderId: "509489897704"
+};
+firebase.initializeApp(config);
+
+
+var database = firebase.database();
+var storage = firebase.storage().ref();
+var user = firebase.auth().currentUser;
+
+var ref = database.ref('user');
+ref.on('value',gotData,errData)
+
+function gotData(data) {
+  console.log(data.val())
+  var scores = data.val();
+  var keys = Object.keys(scores);
+  console.log(keys);  
+
+  console.log(scores[keys[0]].user)
+}
+function errData(err) {
+  console.log('Error!')
+  console.log(err);
+}
+
+
+// database.ref().on("child_added", function(childsnapshot) {
+
+//   var user = childsnapshot.val().user;
+//   var profileAge = childsnapshot.val().profileAge;
+//   var profileEmail = childsnapshot.val().profileEmail;
+//   var profileFirstName = childsnapshot.val().profileFirstName;
+//   var profileLastName = childsnapshot.val().profileLastName;
+//   var profileGender = childsnapshot.val().profileGender;
+//   var profileCity = childsnapshot.val().profileCity;
+//   var profileState = childsnapshot.val().profileState;
+  
+//   $("#age").html("Age: " + profileAge);
+//   $("#location").html("Location: " + profileCity + ", " + profileState);
+//   $("#gender").html("Gender: " + profileGender);
+//   $("#name").html("Name: " + profileFirstName + " " + profileLastName)
+
+//   if (firebase.auth().currentUser.uid !== user) {
+//     $("#hate-content").append(
+//     "<tr><td class='box'>" + profileFirstName + "</td>"
+//     +"<td>" + profileLastName + "</td>"
+//     +"<td class='box'>" + profileGender + "</td>"
+//     +"<td>" + profileAge + "</td>"
+//     +"<td class='box'>" + "<button type='button' class='btn btn-info btn-lg' data-toggle='modal' data-target='#myModal'></button>" + "</td></tr>");
+
+//       // profileFirstName +" "+ profileLastName +"<br>" + profileGender + ", " + profileAge)
+//   }
+
+
+// });
+
+$("#add-profile").on("click", function() {
+  var questionVal = false;
+  var profileAge = $("#profile-age").val().trim().toUpperCase();
+  var profileEmail = $("#profile-email").val().trim();
+  var profileFirstName = $("#profile-first-name").val().trim().toUpperCase();
+  var profileLastName = $("#profile-last-name").val().trim().toUpperCase();
+  var profileGender = $("#profile-gender").val().trim().toUpperCase();
+  var profileCity = $("#profile-city").val().trim().toUpperCase();
+  var profileState = $("#profile-state").val().trim().toUpperCase();
+  var user = firebase.auth().currentUser.uid;
+  var pushedAnswers = [];
+
+  var newProfile = {
+    user: user,
+    profileEmail: profileEmail,
+    profileAge: profileAge,
+    profileFirstName: profileFirstName,
+    profileLastName: profileLastName,
+    profileGender: profileGender,
+    profileCity: profileCity,
+    profileState: profileState,
+    hates: pushedAnswers
+  }
+
+  database.ref().child('user').push(newProfile);
+});
+
+
+var ref = firebase.database().ref('user');
+ref.on("child_added", function(childsnapshot) {
+  var user = childsnapshot.val().user;
+  var profileAge = childsnapshot.val().profileAge;
+  var profileEmail = childsnapshot.val().profileEmail;
+  var profileFirstName = childsnapshot.val().profileFirstName;
+  var profileLastName = childsnapshot.val().profileLastName;
+  var profileGender = childsnapshot.val().profileGender;
+  var profileCity = childsnapshot.val().profileCity;
+  var profileState = childsnapshot.val().profileState;
+  
+  if ( firebase.auth().currentUser.uid === user ) {
+    $("#age").html("Age: " + profileAge);
+    $("#location").html("Location: " + profileCity + ", " + profileState);
+    $("#gender").html("Gender: " + profileGender);
+    $("#name").html("Name: " + profileFirstName + " " + profileLastName)
+    console.log(user)
+    console.log(profileFirstName);
+    $("#profile-info").hide();
+  }
+});
+
+var ref = firebase.database().ref('hates');
+ref.on("child_added", function(childsnapshot) {
+  var user = childsnapshot.val().user;
+  var profileAge = childsnapshot.val().newAge;
+  var profileEmail = childsnapshot.val().email;
+  var profileFirstName = childsnapshot.val().newFirstName;
+  var profileLastName = childsnapshot.val().newLastName;
+  var profileGender = childsnapshot.val().newGender;
+  var profileCity = childsnapshot.val().newCity;
+  var profileState = childsnapshot.val().newState;
+  var hates = childsnapshot.val().hates;
+  console.log(hates)
+
+
+
+ if (firebase.auth().currentUser.uid !== user) {
+    $("#hate-content").append(
+    "<tr><td>" + profileFirstName +" "+profileLastName + "</td>"
+    +"<td>" + profileGender + "</td>"
+    +"<td>" + profileAge + "</td>"
+    +"<td>" + hates + "</td></tr>")
+    // +"<td>" + "<button type='button' class='btn btn-info' data-toggle='modal' data-target='#myModal'>"+"Hates"+"</button>" + "</td></tr>");
+    
+    $("#modal-body").append(hates);
+    
+    }
+ });
+
+
+
+// var ref = firebase.database().ref('hates');
+// ref.on("child_added", function(childsnapshot) {
+//   var user = childsnapshot.val().user;
+//   var hates = childsnapshot.val().hates;
+
+//   if (firebase.auth().currentUser.uid !== user) {
+//     for (var i = 0; i < keys.length; i++) {
+//       var k = keys[i];
+
+//       $(".modal-body").append(hates)
+//     }
+//     }
+// });
+
+
+var selectedFile;
+
+$("#imgFile").on("change", function(event){
+    selectedFile = event.target.files[0];
+});
+
+function uploadFile(){
+  var filename = selectedFile.name;
+  var storageRef = firebase.storage().ref("profile-images/" + filename);
+
+  var metadata = {
+    contentType: 'image/jpeg',
+  };
+
+  // Upload the file and metadata
+  var uploadTask = storageRef.put(selectedFile, metadata);
+
+uploadTask.on("state-changed", function(snapshot){
+
+  }, function(error){
+
+  }, function(){
+
+    var downloadURL = uploadTask.snapshot.downloadURL;
+    console.log(downloadURL);
+});
+}
+
+// var storage = firebase.storage();
+// var storageRef = storage.ref();
+// var tangRef = storageRef.child('profile-images');
+
+//   // Once the sign in completed, we get the download URL of the image
+//   tangRef.getDownloadURL().then(function(url)                             {
+//     // Once we have the download URL, we set it to our img element
+//     document.querySelector('img').src = url;
+
+//   }).catch(function(error) {
+//     // If anything goes wrong while getting the download URL, log the error
+//     console.error(error);
+//   });
+
+$("#add-location").on("click", function(event) {
+
+event.preventDefault();
+
+// This line will grab the text from the input box
+var restaurant = $("#location-input").val().trim();
+// The movie from the textbox is then added to our array
+
+var queryURL = "https://developers.zomato.com/api/v2.1/search?entity_id="+restaurant+
+"&apikey=b79214ee05ebf45e51ded6595e897f8d";
+
+var queryURL = "https://developers.zomato.com/api/v2.1/search?entity_id="+restaurant+"&apikey=b79214ee05ebf45e51ded6595e897f8d";
+
+// Here we run our AJAX call to the OpenWeatherMap API
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+// We store all of the retrieved data inside of an object called "response"
+.done(function(response) {
+
+console.log(response);
+
+console.log(response.restaurants[Math.floor((Math.random() * 20) + 1)].restaurant.name);
+
+var restaurantResults = response.restaurants[Math.floor((Math.random() * 20) + 1)].restaurant.name;
+
+
+$("#restaurantDisplay").html(restaurantResults);
+
+
+});
+});
--- survey.js
+++ survey.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
   function initialScreen(){
     startScreen= "<img class='center-block' src='redheart.jpg'>"
-    + "<p class='text-center main-button-container'><a class= 'btn btn-primary btn-lg start-button'>Start</a></p>";
+    + "<p class='text-center main-button-container'><a class= 'btn btn-primary btn-lg btn-block start-button'>Start</a></p>";
     $(".mainArea").html(startScreen);
   }
   initialScreen();
@@ -13,55 +13,89 @@ $('body').on('click', '.start-button', function(){
   event.preventDefault();
   displayButtons();
   createSubmitBtn();
-  $('body').addClass('survey');
+
 
 function displayButtons(){
   $('.mainArea').empty(); //this empties the mainArea so buttons can go there
   for(var i = 0; i < questions.length; i++){
     var button = $("<div>");
-    button.attr("value", 1);
-    button.addClass("btn btn-danger btn-lg btn-block");
+    button.attr("value", i);
+    button.addClass("btn btn-danger btn-block btn-lg");
     button.text( questions[i]);
     button.attr("data-name", questions[i]);
     $('.mainArea').append(button);
-
   }
 }
 
 function createSubmitBtn() {
-  submitButton= "<p class='text-center' ><a class='btn btn-success btn-lg submit-button'>Submit!</a></p>";
+  submitButton= "<p class='text-center' ><a class='btn btn-success btn-lg btn-block submit-button'>Submit!</a></p>";
   $(".submitBtn").append(submitButton);
 }
 });
 
 $('.submitBtn').on('click', function(){
 
-  $('.btn-primary').each(function(){
-    console.log(this);
-    var buttonText = $(this).attr("data-name");
-    //push clicked-button text into firebase
-    database.ref().push(buttonText);
-    })
+
+  var ref = database.ref('user');
+    ref.on('value',gotData)
+
+  function gotData(data) {
+    console.log(data.val())
+    var hates = data.val();
+    var keys = Object.keys(hates);
+    console.log(keys);  
+    console.log(hates[keys[0]].hates)
+
+    var user = firebase.auth().currentUser.uid;
+    var email = firebase.auth().currentUser.email;
+    var newFirstName = hates[keys[keys.length-1]].profileFirstName
+    var newLastName = hates[keys[keys.length-1]].profileLastName
+    var newGender = hates[keys[keys.length-1]].profileGender
+    var newCity = hates[keys[keys.length-1]].profileCity
+    var newState = hates[keys[keys.length-1]].profileState
+    var newAge = hates[keys[keys.length-1]].profileAge
+
+    var pushed = {
+      hates : pushedAnswers,
+      user:user,
+      email:email,
+      newFirstName: newFirstName,
+      newState: newState,
+      newCity: newCity,
+      newGender: newGender,
+      newLastName: newLastName,
+      newAge: newAge
+    }
+
+  database.ref().child('hates').push(pushed);
+  }
+  $("#mainArea").hide();
 })
 
 //Initialize Firebase
 var config = {
-    apiKey: "AIzaSyD05-stFAPDBOgjBtXNZt7VrqoEo8GcTNU",
-    authDomain: "hatedate-a1278.firebaseapp.com",
-    databaseURL: "https://hatedate-a1278.firebaseio.com",
-    projectId: "hatedate-a1278",
-    storageBucket: "",
-    messagingSenderId: "506193273181"
-  };
-
+  apiKey: "AIzaSyCsk5WGqR1YlbaJiCnYbs1lgPgEQZAopr8",
+  authDomain: "hate-date.firebaseapp.com",
+  databaseURL: "https://hate-date.firebaseio.com",
+  projectId: "hate-date",
+  storageBucket: "hate-date.appspot.com",
+  messagingSenderId: "509489897704"
+};
 firebase.initializeApp(config);
+
 
 var database = firebase.database();
 
-$(".mainArea").on('click', '.btn-danger, .btn-primary', function(event){
+$(".mainArea").on('click', '.btn-danger', function(event){
     // event.preventDefault();
     $(this).toggleClass("btn-primary btn-danger");
     console.log("You clicked: " + $(this).attr("data-name"));
+	  
+		//push clicked-button text into firebase
+		// database.ref.(buttonText);
+    var buttonPush = $(this).attr("data-name");
+    pushedAnswers.push(buttonPush)
+    console.log(pushedAnswers)
 
 });
 
@@ -71,35 +105,31 @@ var pushedAnswers =[];
 
 //global variabls go here
 var questions = [
-  "404 errors",
-	"Side hugs",
-	"Rudeness",
-	"Hipsters",
-	"GMO food",
-  "Bad listeners",
-	"Flakey people",
-	"Selfie sticks",
-	"Slow walkers",
-	"Justin Bieber",
-	"Donald Trump",
-	"Picky Eaters",
-	"Hot weather",
-	"Mean girls",
-	"Couples that wear matching outfits",
-	"People who text during movies",
-  "Paying extra for almond milk",
-	"Too much cologne or perfume",
-	"Pharmaceutical companies",
-  "People who don't use indicator lights",
   "Useless noises like chewing, humming, whistling",
 	"The phrase, 'everything happens for a reason'",
 	"Parents who don't discipline their children",
 	"People who are on their phones all the time",
-	"People who don't believe in climate change"
+	"People who don't believe in climate change",
+	 "People who don't use indicator lights",
+	"Couples that wear matching outfits",
+	"People who text during movies",
+	 "Too much cologne or perfume",
+	"Pharmaceutical companies",
+	//10 mark
+	"Bad listeners",
+	"Flakey people",
+	"Selfie sticks",
+	"Slow walkers",
+	 "Justin Bieber",
+	"Donald Trump",
+	"Picky Eaters",
+	"Hot weather",
+	"Mean girls",
+	"Paying extra for almond milk",
+	//20 mark
+	"404 errors",
+	"Side hugs",
+	"Rudeness",
+	"Hipsters",
+	"GMO food"
 ]
-//assign false value to each question
-// var qAnswer = [];
-//
-// for(var i = 0; i < questions.length; i++){
-// 	qAnswer[i] = false;
-// }
